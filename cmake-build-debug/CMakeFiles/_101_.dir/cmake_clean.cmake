8662ac5e626ec6c6947e7d9ae6ecd778321c0202
xujianwei <623707810@qq.com>
2020-08-04 17:11:36 +0800

    222
    111

---
+++ cmake-build-debug/CMakeFiles/_101_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_101_.dir/BFS/_101_Symmetric_Tree/Solution.cpp.obj"
+  "_101_.exe"
+  "_101_.exe.manifest"
+  "_101_.lib"
+  "_101_.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_101_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/_28_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_28_.dir/Strings/_28__Implement_strStr()/Solution.cpp.obj"
+  "_28_.exe"
+  "_28_.exe.manifest"
+  "_28_.lib"
+  "_28_.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_28_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
