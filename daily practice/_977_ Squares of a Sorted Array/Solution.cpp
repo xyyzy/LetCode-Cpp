2681c4cbbc1bc6fe1602c049cfd834f67d073ace
xujianwei <623707810@qq.com>
2020-10-16 13:25:44 +0800

    977

---
+++ daily practice/_977_ Squares of a Sorted Array/Solution.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Xulin on 2020/10/16.
+//
+
+#include "Solution.h"
---
+++ daily practice/_977_ Squares of a Sorted Array/Solution.h
@@ -0,0 +1,24 @@
+//
+// Created by Xulin on 2020/10/16.
+//
+
+#ifndef LETCODEC___SOLUTION_H
+#define LETCODEC___SOLUTION_H
+#include <vector>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+
+
+class Solution {
+    vector<int> sortedSquares(vector<int>& A) {
+        for (int i = 0; i < A.size(); ++i) {
+            A[i]=pow(A[i],2);
+        }
+        sort(A.begin(),A.end());
+        return A;
+    }
+};
+
+
+#endif //LETCODEC___SOLUTION_H
