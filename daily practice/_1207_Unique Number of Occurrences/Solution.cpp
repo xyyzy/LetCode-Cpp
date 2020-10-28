b92ea2994a963fd50cef4ab86de5827a6e0c93a2
xujianwei <623707810@qq.com>
2020-10-28 12:05:10 +0800

    845. 数组中的最长山脉

---
+++ daily practice/_1207_Unique Number of Occurrences/Solution.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Xulin on 2020/10/28.
+//
+
+#include "Solution.h"
---
+++ daily practice/_1207_Unique Number of Occurrences/Solution.h
@@ -0,0 +1,36 @@
+//
+// Created by Xulin on 2020/10/28.
+//
+
+#ifndef LETCODEC___SOLUTION_H
+#define LETCODEC___SOLUTION_H
+
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    //哈希表
+    bool uniqueOccurrences(vector<int> &arr) {
+        unordered_map<int, int> occur;
+        for (const auto &x:arr) {
+            occur[x]++;
+        }
+        unordered_set<int> time;
+        /*for (const auto &x:occur) {
+            time.insert(x.second);
+        }*/
+        //也可
+        for(pair<int,int> i :occur){
+            time.insert(i.second);
+        }
+        return time.size() == occur.size();
+    }
+};
+
+
+#endif //LETCODEC___SOLUTION_H
